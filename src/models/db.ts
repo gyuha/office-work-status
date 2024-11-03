8b86e8ef1c21c9c9718b1b498a125073a95786a3
gyuha <nicegyuha@gmail.com>
2024-11-03 19:46:15 +0900

    reset

--- src/models/db.ts
+++
@@ -1,9 +0,0 @@
-import sqlite3 from "sqlite3";
-import { open, Database } from "sqlite";
-
-export async function openDb(): Promise<Database> {
-    return open({
-        filename: "./database.db",
-        driver: sqlite3.Database,
-    });
-}
